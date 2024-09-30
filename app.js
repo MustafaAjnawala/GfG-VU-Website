b7eaa8e7dd40faf4d034e1371b7f95b86a03ee67
MustafaAjnawala <mustafaajnawala2004@gmail.com>
2024-09-30 11:33:53 +0530

    cleaning

--- app.js
+++ app.js
@@ -23,159 +23,153 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const pool = mysql.createPool({
   connectionLimit: 5,
-  port : process.env.SQLPORT,
+  port: process.env.SQLPORT,
   host: process.env.MYSQL_URL,
   user: process.env.U,
   password: process.env.PWDD,
   database: process.env.DB
-  
-});
-// console.log("SQLPORT:", process.env.SQLPORT);
-// console.log("MYSQL_URL:", process.env.MYSQL_URL);
-// console.log("USERNAME:", process.env.U);
-// console.log("PWD:", process.env.PWDD);
-// console.log("DB:", process.env.DB);
 
-app.get('/',(req,res)=>{
+});
+
+app.get('/', (req, res) => {
   sec = 0;
-  pool.query('select * from Upcoming_events',(error,upcomingResults)=>{
-    if(error){
-      console.error('Error fetching upcoming events :',error);
+  pool.query('select * from Upcoming_events', (error, upcomingResults) => {
+    if (error) {
+      console.error('Error fetching upcoming events :', error);
       return res.status(500).send('Internal Server Error');
     }
-    pool.query('Select * from past_events',(error,pastResults)=>{
-      if(error){
-        console.error('Error fetching past events :',error);
+    pool.query('Select * from past_events', (error, pastResults) => {
+      if (error) {
+        console.error('Error fetching past events :', error);
         return res.status(500).send('Internal Server Error');
       }
-      res.render('index',{upcomingEvents: upcomingResults,pastEvents:pastResults});
+      res.render('index', { upcomingEvents: upcomingResults, pastEvents: pastResults });
     });
   });
 });
 
 app.post('/login', (req, res) => {
-    let { username, password } = req.body;
+  let { username, password } = req.body;
 
-    // Sanitizing both parameters from harmful HTML tags
-    username = sanitizeHtml(username);
-    password = sanitizeHtml(password);
+  // Sanitizing both parameters from harmful HTML tags
+  username = sanitizeHtml(username);
+  password = sanitizeHtml(password);
 
-    pool.query('SELECT * FROM login WHERE username = ?', [username], (error, results) => {
-        if (error) {
-            console.error('Error:', error);
-            //500 error status is for internal server error
-            res.status(500).send('Internal Server Error');
+  pool.query('SELECT * FROM login WHERE username = ?', [username], (error, results) => {
+    if (error) {
+      console.error('Error:', error);
+      res.status(500).send('Internal Server Error');
+    } else {
+      if (results.length > 0) {
+        const userData = results[0];
+        if (password === userData.password) {
+          // Login successful
+          const { username, name } = userData;
+          sec = 1;
+          return res.redirect('/panel');
         } else {
-            if (results.length > 0) {
-                const userData = results[0];
-                if (password === userData.password) {
-                    // Login successful
-                    const { username, name } = userData;
-                    sec = 1;
-                    return res.redirect('/panel');
-                } else {
-                  return res.status(401).send('Invalid username or password');
-                }
-            } else {
-                return res.send(`<script>alert('Invalid username or password... :(');window.location.href = '/';</script>`);
-            }
+          return res.status(401).send('Invalid username or password');
         }
-    });
+      } else {
+        return res.send(`<script>alert('Invalid username or password... :(');window.location.href = '/';</script>`);
+      }
+    }
+  });
 });
 
 app.get('/panel', (req, res) => {
-  if(sec===1){
+  if (sec === 1) {
 
-  pool.query('SELECT * FROM Upcoming_events', (error, upcomingEvents) => {
-    if (error) {
-      console.error('Error fetching events:', error);
-      return res.status(500).send('Internal Server Error');
-    }
-    pool.query('SELECT * FROM past_events', (error, pastEvents) => {
+    pool.query('SELECT * FROM Upcoming_events', (error, upcomingEvents) => {
       if (error) {
-        console.error('Error fetching past events:', error);
+        console.error('Error fetching events:', error);
         return res.status(500).send('Internal Server Error');
       }
-      res.render('panel', { upcomingEvents: upcomingEvents, pastEvents: pastEvents });
-    });
-  });
-}
-else{
-  return res.status(500).send('Unauthorized Access');
-}
-});
-
-  //post request to add a new event into the list
-  app.post('/add-event',upload.single('eventImage'),(req, res) => {
-    const { eventName, eventDesc, eventDate, eventTime, eventReg } = req.body;
-    const eventImage = req.file ? req.file.buffer : null;
-
-    const sanitizedEventName = sanitizeHtml(eventName);
-    const sanitizedEventDesc = sanitizeHtml(eventDesc);
-    const sanitizedEventDate = sanitizeHtml(eventDate);
-    const sanitizedEventTime = sanitizeHtml(eventTime);
-    const sanitizedEventReg = sanitizeHtml(eventReg);
-  
-    pool.query(
-     'INSERT INTO Upcoming_events (eventName, eventDesc, eventDate, eventTime, eventReg, eventImage) VALUES (?, ?, ?, ?, ?, ?)',
-    [sanitizedEventName, sanitizedEventDesc, sanitizedEventDate, sanitizedEventTime, sanitizedEventReg, eventImage],
-      (error, results) => {
+      pool.query('SELECT * FROM past_events', (error, pastEvents) => {
         if (error) {
-          console.error('Error adding event:', error);
+          console.error('Error fetching past events:', error);
           return res.status(500).send('Internal Server Error');
         }
-        res.redirect('/panel');
-      }
-    );
-  });
+        res.render('panel', { upcomingEvents: upcomingEvents, pastEvents: pastEvents });
+      });
+    });
+  }
+  else {
+    return res.status(500).send('Unauthorized Access');
+  }
+});
 
-  //to delete the respective upcoming event and details
-  app.post('/delete-event', (req, res) => {
-    const { eventID } = req.body;
-  
-    pool.query('DELETE FROM Upcoming_events WHERE eventID = ?', [eventID], (error, results) => {
+//post request to add a new event into the list
+app.post('/add-event', upload.single('eventImage'), (req, res) => {
+  const { eventName, eventDesc, eventDate, eventTime, eventReg } = req.body;
+  const eventImage = req.file ? req.file.buffer : null;
+
+  const sanitizedEventName = sanitizeHtml(eventName);
+  const sanitizedEventDesc = sanitizeHtml(eventDesc);
+  const sanitizedEventDate = sanitizeHtml(eventDate);
+  const sanitizedEventTime = sanitizeHtml(eventTime);
+  const sanitizedEventReg = sanitizeHtml(eventReg);
+
+  pool.query(
+    'INSERT INTO Upcoming_events (eventName, eventDesc, eventDate, eventTime, eventReg, eventImage) VALUES (?, ?, ?, ?, ?, ?)',
+    [sanitizedEventName, sanitizedEventDesc, sanitizedEventDate, sanitizedEventTime, sanitizedEventReg, eventImage],
+    (error, results) => {
       if (error) {
-        console.error('Error deleting event:', error);
+        console.error('Error adding event:', error);
         return res.status(500).send('Internal Server Error');
       }
       res.redirect('/panel');
-    });
+    }
+  );
+});
+
+//to delete the respective upcoming event and details
+app.post('/delete-event', (req, res) => {
+  const { eventID } = req.body;
+
+  pool.query('DELETE FROM Upcoming_events WHERE eventID = ?', [eventID], (error, results) => {
+    if (error) {
+      console.error('Error deleting event:', error);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/panel');
   });
+});
 
-  //post req to add an image to the past event list
-  app.post('/add-past-event',upload.single('eventImage'),(req, res) => {
-    const eventImage = req.file ? req.file.buffer : null;
+//post req to add an image to the past event list
+app.post('/add-past-event', upload.single('eventImage'), (req, res) => {
+  const eventImage = req.file ? req.file.buffer : null;
 
-    pool.query(
-     'INSERT INTO past_events (PastEventImage) VALUES (?)',
+  pool.query(
+    'INSERT INTO past_events (PastEventImage) VALUES (?)',
     [eventImage],
-      (error, results) => {
-        if (error) {
-          console.error('Error adding event:', error);
-          return res.status(500).send('Internal Server Error');
-        }
-        res.redirect('/panel');
+    (error, results) => {
+      if (error) {
+        console.error('Error adding event:', error);
+        return res.status(500).send('Internal Server Error');
       }
-    );
+      res.redirect('/panel');
+    }
+  );
+});
+
+//to delete the respective past event image
+app.post('/delete-past-event', (req, res) => {
+  const { eventID } = req.body;
+
+  pool.query('DELETE FROM past_events WHERE PastEventID = ?', [eventID], (error, results) => {
+    if (error) {
+      console.error('Error deleting event:', error);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/panel');
   });
+});
 
-    //to delete the respective past event image
-    app.post('/delete-past-event', (req, res) => {
-      const { eventID } = req.body;
-    
-      pool.query('DELETE FROM past_events WHERE PastEventID = ?', [eventID], (error, results) => {
-        if (error) {
-          console.error('Error deleting event:', error);
-          return res.status(500).send('Internal Server Error');
-        }
-        res.redirect('/panel');
-      });
-    });
-
-app.post('/logout',(req,res)=>{
+app.post('/logout', (req, res) => {
   res.redirect("/");
 });
 
 app.listen(port, () => {
-    console.log(`Server is up annd running on http://localhost:${port}`);
+  console.log(`Server is up annd running on http://localhost:${port}`);
 });
